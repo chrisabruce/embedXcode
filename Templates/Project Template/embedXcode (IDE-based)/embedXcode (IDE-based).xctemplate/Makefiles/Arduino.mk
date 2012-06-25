d8388bbc683f41a0de9ea13fc499e258a50368ba
Rei Vilo <rei_vilo@yahoo.fr>
2012-06-25 17:24:50 +0200

    Jun 25, 2012 release 14 • HEX and BIN size optimisation

--- Templates/Project Template/embedXcode (IDE-based)/embedXcode (IDE-based).xctemplate/Makefiles/Arduino.mk
+++ Templates/Project Template/embedXcode (IDE-based)/embedXcode (IDE-based).xctemplate/Makefiles/Arduino.mk
@@ -18,10 +18,8 @@
 # Automatic 0023 or 1.0 selection based on version.txt
 #
 ifneq ($(shell grep 1.0 $(ARDUINO_PATH)/lib/version.txt),)
-    $(info 1.0)
     include $(MAKEFILE_PATH)/Arduino1.mk	
 else
-    $(info 23)
     include $(MAKEFILE_PATH)/Arduino23.mk	
 endif
 
